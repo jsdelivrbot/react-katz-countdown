fea17a836e0a0bea89883a4bbcdfb057cd030b06
boadmin <erik@bodigiti.com>
2017-05-11 17:08:54 -0400

    Through LESSON 16 -- Creating a Clock Component.

--- bundle.js
+++ bundle.js
@@ -19783,6 +19783,10 @@
 
 	var _react2 = _interopRequireDefault(_react);
 
+	var _Clock = __webpack_require__(161);
+
+	var _Clock2 = _interopRequireDefault(_Clock);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -19831,30 +19835,7 @@
 	          'Countdown to ',
 	          this.state.deadline
 	        ),
-	        _react2.default.createElement(
-	          'div',
-	          null,
-	          _react2.default.createElement(
-	            'div',
-	            { className: 'Clock-days' },
-	            '14 days'
-	          ),
-	          _react2.default.createElement(
-	            'div',
-	            { className: 'Clock-hours' },
-	            '30 hours'
-	          ),
-	          _react2.default.createElement(
-	            'div',
-	            { className: 'Clock-minutes' },
-	            '15 minutes'
-	          ),
-	          _react2.default.createElement(
-	            'div',
-	            { className: 'Clock-seconds' },
-	            '20 seconds'
-	          )
-	        ),
+	        _react2.default.createElement(_Clock2.default, null),
 	        _react2.default.createElement(
 	          'div',
 	          null,
@@ -19880,6 +19861,86 @@
 	}(_react.Component);
 
 	exports.default = App;
+
+/***/ }),
+/* 161 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(2);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var Clock = function (_Component) {
+	  _inherits(Clock, _Component);
+
+	  function Clock(props) {
+	    _classCallCheck(this, Clock);
+
+	    var _this = _possibleConstructorReturn(this, (Clock.__proto__ || Object.getPrototypeOf(Clock)).call(this, props));
+
+	    _this.state = {
+	      days: 0,
+	      hours: 0,
+	      minutes: 0,
+	      seconds: 0
+	    };
+	    return _this;
+	  }
+
+	  _createClass(Clock, [{
+	    key: "render",
+	    value: function render() {
+	      return _react2.default.createElement(
+	        "div",
+	        null,
+	        _react2.default.createElement(
+	          "div",
+	          { className: "Clock-days" },
+	          this.state.days,
+	          " days"
+	        ),
+	        _react2.default.createElement(
+	          "div",
+	          { className: "Clock-hours" },
+	          this.state.hours,
+	          " hours"
+	        ),
+	        _react2.default.createElement(
+	          "div",
+	          { className: "Clock-minutes" },
+	          this.state.minutes,
+	          " minutes"
+	        ),
+	        _react2.default.createElement(
+	          "div",
+	          { className: "Clock-seconds" },
+	          this.state.seconds,
+	          " seconds"
+	        )
+	      );
+	    }
+	  }]);
+
+	  return Clock;
+	}(_react.Component);
+
+	exports.default = Clock;
 
 /***/ })
 /******/ ]);
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Clock from './Clock';
 
 class App extends Component {
   // State -- Add/Set
@@ -23,12 +24,7 @@ class App extends Component {
         <div className="App-title">
           Countdown to {this.state.deadline}
         </div>
-        <div>
-          <div className="Clock-days">14 days</div>
-          <div className="Clock-hours">30 hours</div>
-          <div className="Clock-minutes">15 minutes</div>
-          <div className="Clock-seconds">20 seconds</div>
-        </div>
+        <Clock />
         <div>
           <input 
             placeholder='new date'
