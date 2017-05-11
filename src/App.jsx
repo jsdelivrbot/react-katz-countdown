0d1d7d085007a8035c380a355897a48aa2185969
boadmin <erik@bodigiti.com>
2017-05-11 09:00:52 -0400

    First React Component.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Coundown Champ, App Component</div>
+    )
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './App';
+
 
 ReactDOM.render(
-  <h1>Hello</h1>,
+  <App />,
   document.getElementById('root-01')
 );
